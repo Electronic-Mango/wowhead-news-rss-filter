212cd6caabc4121806416c17b8f0396617c01620
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2023-06-02 14:40:31 +0200

    Add new endpoint forwarding unmodified news

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from requests import get
+
+NEWS_RSS_URL = "https://www.wowhead.com/news&rss"
 
 app = FastAPI()
 
@@ -6,3 +9,9 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/news")
+async def news():
+    response = get(NEWS_RSS_URL)
+    return response.text
