8d0aab2c94fb02e0233ec0361f8224dc8f28b98a
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2023-06-02 14:50:14 +0200

    Allow filtering out entries via query parameters

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from typing import Annotated
+from xml.etree.ElementTree import fromstring, tostring
+
+from fastapi import FastAPI, Query
 from requests import get
 
 NEWS_RSS_URL = "https://www.wowhead.com/news&rss"
@@ -12,6 +15,13 @@ async def root():
 
 
 @app.get("/news")
-async def news():
-    response = get(NEWS_RSS_URL)
-    return response.text
+async def news(remove: Annotated[list[str], Query()] = None):
+    response = get(NEWS_RSS_URL).text
+    if not remove:
+        return response
+    xml = fromstring(response)
+    channel = xml.find("channel")
+    for item in channel.findall("item"):
+        if item.find("category").text in remove:
+            channel.remove(item)
+    return tostring(xml)
